c64d3c0949bc20c0a20180260140e668c2a6bf87
Jesse Chen <jchen428@gatech.edu>
2015-02-11 17:31:05 -0500

    lab10

---
+++ Labs/lab10/mult4.asm
@@ -0,0 +1,9 @@
+.orig x3000
+
+  ;write your code here!
+
+  HALT
+
+  N .fill 20
+  ANS .fill -1
+.end
---
+++ Labs/lab10/negate.asm
@@ -0,0 +1,10 @@
+.orig x3000
+
+  ;WRITE YOUR CODE HERE!
+
+  HALT
+
+  N .fill 50
+  ANS .fill 0
+
+.end
