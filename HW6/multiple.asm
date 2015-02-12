9c2f04c76716d2839571383245d047424ff6b280
Jesse Chen <jchen428@gatech.edu>
2015-02-12 13:26:13 -0500

    multiple

--- HW6/multiple.asm
+++ HW6/multiple.asm
@@ -4,7 +4,13 @@
 
 .orig x3000
 
-; CODE GOES HERE! :D
+LOOP    LD R0, A        ;Load A into R0
+        LD R1, B        ;Load B into R1
+        LD R2, ANSWER   ;Load ANSWER into R2
+        NOT R1, R1      ;B = !B begin 2's complement
+        ADD R1, R1, 1   ;B++ finish 2's complement
+        ADD R0, R0, R1  ;Check A > B
+        BRnz LOOP       ;Loop back to beginning
 	
 A       .fill 25
 B       .fill 5
