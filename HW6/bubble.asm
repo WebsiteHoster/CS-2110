7e0dfcd6054e1612beeb1718ddf700f4f985432c
Jesse Chen <jesse.l.chen@gmail.com>
2015-02-17 15:43:40 -0500

    Fixed intialization of K

--- HW6/bubble.asm
+++ HW6/bubble.asm
@@ -4,38 +4,37 @@
 
 .orig x3000
 
-		LD R2, NUMBER		;Load NUMBER into R2
-		NOT R2, R2			;Two's complement
-		ADD R2, R2, 1 		;-NUMBER
-
 		LD R6, ARRAY		;Load ARRAY (x6000) starting index into R6
-		ADD R6, R6, -1
-		AND R5, R5, 0		;Initialize count in R5
-		AND R0, R0, 0		;Initialize for-loop index K in R0
+		ADD R6, R6, -1 		;Start at x5FFF
+		AND R0, R0, 0		;Initialize LOOP1 index K in R0
 		LD R1, LENGTH		;Load LENGTH (for-loop condition) into R1
 		NOT R1, R1			;Two's complement
 		ADD R1, R1, 1 		;-LENGTH
 
-LOOP	ST R0, K			;Store R0 in K
+LOOP1	ST R0, K			;Store R0 in K
 		ADD R0, R0, R1		;K - LENGTH
 		BRzp BREAK			;If K < LENGTH -> break
 
-		LDR R3, R6, 1		;Load data from ARRAY[K] into R3
-		ADD R3, R3, R2		;ARRAY[K] - NUMBER
-		BRnp ELSE			;If false, skip to next iteration
-		ADD R5, R5, 1 		;If true, increment count
+		AND R2, R2, 0		;Initialize/reset isSorted
+		ADD R2, R2, 1 		;isSorted = 1
+
+		AND R3, R3, 0		;Initialize LOOP2 index I in R3 to 1
+		ADD R3, R3, 1
+
+LOOP2	
 
 ELSE	LD R0, K			;Undo two's complement on K
 		ADD R0, R0, 1 		;Increment K
 		ADD R6, R6, 1 		;Increment array index
 
-		BR LOOP				;Repeat loop
+		BR LOOP1			;Repeat loop
 
-BREAK 	ST R5, ANSWER		;Store R5 in ANSWER
+BREAK 	;ST R5, ANSWER		;Store R5 in ANSWER
 
 		HALT
 
 K		.fill 0
+I 		.fill 1
 
 ARRAY   .fill x6000
 LENGTH  .fill 12
--- HW6/count.asm
+++ HW6/count.asm
@@ -11,7 +11,7 @@
 		LD R6, ARRAY		;Load ARRAY (x6000) starting index into R6
 		ADD R6, R6, -1 		;Start at x5FFF
 		AND R5, R5, 0		;Initialize count in R5
-		AND R0, R0, 0		;Initialize for-loop index K in R0
+		LD R0, K			;Initialize for-loop index K in R0
 		LD R1, LENGTH		;Load LENGTH (for-loop condition) into R1
 		NOT R1, R1			;Two's complement
 		ADD R1, R1, 1 		;-LENGTH
