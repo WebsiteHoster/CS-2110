8e7a6564d4ce37e35943eb06c8d0a289637301bb
jchen428 <jesse.l.chen@gmail.com>
2015-02-18 16:38:23 -0500

    timedlab2

---
+++ Labs/timedlab2/traverse.asm
@@ -0,0 +1,26 @@
+;;==============================================
+;; NAME:
+;;==============================================
+
+
+.orig x3000
+
+	; Start your code here
+
+
+MAX_VAL .fill 0			; Store the maximum value in the array at this label
+ODD_CNT	.fill 0			; Store the total number of odd entries in the array at this label
+LENGTH	.fill 0			; Store the length of the array at this label
+
+ARRAY	.fill 1			; The first element of the array
+	.fill 3
+	.fill 7
+	.fill 4
+	.fill 2
+	.fill 9
+	.fill 3
+	.fill 4			; The last element of the array
+	.fill -1		; -1 will follow the last element of the array	
+
+
+.end
