f4675f590bcec19e51fd2114cd59fa9578a52efb
Jesse Chen <jesse.l.chen@gmail.com>
2015-02-26 04:03:00 -0500

    Made template pretty

--- HW7/eval.asm
+++ HW7/eval.asm
@@ -1,4 +1,4 @@
-; Name:
+; Name: Jesse Chen
 
 
 ; Main
@@ -6,40 +6,40 @@
 
 .orig x3000
 
-	LD R6, STACK	; Initialize the stack
+		LD R6, STACK		; Initialize the stack
 
-	LEA R0, STRING	; R0 = &str[0]
-	ADD R1, R0, 0
+		LEA R0, STRING		; R0 = &str[0]
+		ADD R1, R0, 0
 
-SL_LOOP	LDR R2, R1, 0	; \ R1 = strlen(str)
-	BRz SL_END	; |
-	ADD R1, R1, 1	; |
-	BR SL_LOOP	; |
-SL_END	NOT R2, R0	; |
-	ADD R2, R2, 1	; |
-	ADD R1, R1, R2	; /
+SL_LOOP	LDR R2, R1, 0		; \ R1 = strlen(str)
+		BRz SL_END			; |
+		ADD R1, R1, 1		; |
+		BR SL_LOOP			; |
+SL_END	NOT R2, R0			; |
+		ADD R2, R2, 1		; |
+		ADD R1, R1, R2		; /
 
-	ADD R6, R6, -2	; \ R0 = eval(str, len)
-	STR R0, R6, 0	; |
-	STR R1, R6, 1	; |
-	LD R2, EVALPTR	; |
-	JSRR R2		; |
-	LDR R0, R6, 0	; |
-	ADD R6, R6, 3	; /
+		ADD R6, R6, -2		; \ R0 = eval(str, len)
+		STR R0, R6, 0		; |
+		STR R1, R6, 1		; |
+		LD R2, EVALPTR		; |
+		JSRR R2				; |
+		LDR R0, R6, 0		; |
+		ADD R6, R6, 3		; /
 
-	ST R0, ANS
-	HALT
+		ST R0, ANS
+		HALT
 
 STACK	.fill xf000
-ANS	.fill -1
+ANS		.fill -1
 EVALPTR	.fill EVAL
 STRING	.stringz "1*2+3*4+5"
-	.blkw 200
+		.blkw 200
 
 EVAL
 
-	; Write your function here
+		; Write your function here
 
-	RET
+		RET
 
 .end
